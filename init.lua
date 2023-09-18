10c3e21709102e1e179707924e12ed4e9e795c20
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2023-09-19 00:10:55 +1000

    add: relative line numbers and nvim-tree dynamic resizing

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 vim.opt.colorcolumn = "80"
+vim.wo.relativenumber = true
 
+-- resize nvimtree if window got resized
+function NvimTree_width_ratio(percentage)
+  local ratio = percentage / 100
+  local width = math.floor(vim.go.columns * ratio)
+  return width
+end
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = vim.api.nvim_create_augroup("NvimTreeResize", { clear = true }),
+  callback = function()
+    local width = NvimTree_width_ratio(20)
+    vim.cmd("tabdo NvimTreeResize " .. width)
+  end,
+})
