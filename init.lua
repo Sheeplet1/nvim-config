38f6b77e1719f38f673f690ffc1528476b15438a
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2023-09-19 10:31:57 +1000

    add: highlight yanked

--- init.lua
+++ init.lua
@@ -15,3 +15,18 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
     vim.cmd("tabdo NvimTreeResize " .. width)
   end,
 })
+
+-- Highlight yanked group
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+autocmd("TextYankPost", {
+  group = yank_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = "IncSearch",
+      timeout = 40,
+    }
+  end,
+})
