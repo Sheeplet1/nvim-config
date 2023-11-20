ae95359ee692e5e9eb2e8f4b847638bf03c747d1
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2023-11-20 14:06:40 +1100

    add: crates mapping

--- mappings.lua
+++ mappings.lua
@@ -77,4 +77,16 @@ M.dap_python = {
   },
 }
 
+M.crates = {
+  plugin = true,
+  n = {
+    ["<leader>rcu"] = {
+      function()
+        require("crates").upgrade_all_crates()
+      end,
+      "update crates",
+    },
+  },
+}
+
 return M
