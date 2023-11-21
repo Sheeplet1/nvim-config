f45fc2566157a62678c79e0ca19584b850e2a85b
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2023-11-21 19:33:48 +1100

    fix: github copilot

--- plugins.lua
+++ plugins.lua
@@ -191,11 +191,19 @@ local plugins = {
     },
   },
   {
+    -- "github/copilot.vim",
+    -- lazy = false,
+    -- config = function()
+    --   vim.g.copilot_no_tab_map = true
+    --   vim.g.copilot_assume_mapped = true
+    --   vim.g.copilot_tab_fallback = ""
+    -- end,
     "zbirenbaum/copilot.lua",
+    lazy = false,
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
-      require("copilot").setup {}
+      require "custom.configs.copilot"
     end,
   },
   {
